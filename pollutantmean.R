df3cfc21b1efd10fc392ab9cfa6b7e3e9b8482dc
Elise Treit <elisetreit@gmail.com>
2021-07-13 16:34:44 -0700

    fixed indexing issues

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,22 +1,25 @@
-pollutantmean <- function(directory, pollutant, id = 1:332){
+pollutantmean <- function(directory = "specdata", pollutant, id = 1:332){
   #get vector of all means in directory
   pol_mean <- numeric(length = length(id))
+  #this index thing is kinda hacky, try to fix later
+  index = 1
   for(i in id){
-    path <- get_path(directory, i)
+    path <- get_path(i, directory)
     dat <- read.csv(path)
-    pol_mean[i] <- mean(dat[, pollutant], na.rm = TRUE)
+    pol_mean[index] <- mean(dat[, pollutant], na.rm = TRUE)
+    index = index + 1
   }
   #get average of vector
   mean(pol_mean, na.rm = TRUE)
 }
 
-get_path <- function(directory, id){
-  ival <- if(i<10){
-    paste('00', i, sep = "")
-  } else if (i < 100){
-    paste('0', i, sep = "")
+get_path <- function(file_n, directory = "specdata"){
+  ival <- if(file_n<10){
+    paste('00', file_n, sep = "")
+  } else if (file_n < 100){
+    paste('0', file_n, sep = "")
   } else {
-    i
+    file_n
   }
   paste(directory, "/", ival, ".csv", sep = "")
 }
